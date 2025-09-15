fc299b5882ee596764ef9964404567a4c0f810f4
Jeremy Cribb <cribb.jeremy@gmail.com>
2025-09-15 08:27:58 -0400

    Added classes for asteroid and asteroid field.

---
+++ asteroid.py
@@ -0,0 +1,24 @@
+from constants import *
+from circleshape import *
+import pygame
+
+class Asteroid(CircleShape):
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, 
+                           color="white", 
+                           center=(self.x, self.y), 
+                           radius=self.radius,
+                           width=2)
+        
+    # Override the update() method so that it moves in a straight line at constant speed. 
+    # On each frame, it should add (self.velocity * dt) to its position (get self.velocity from
+    # its parent class, CircleShape).
+    def update(self, dt):
+        self.x += self.velocity * dt
+        self.y += self.velocity * dt
+        pass
+
